f92878edd8574ff2e57735f8bd433670e4dbd116
Birxy12 <birxy12@gmail.com>
2026-08-12 02:03:11 +0100

    update: footer social links for WhatsApp and Facebook

--- src/components/MainFooter.jsx
+++ src/components/MainFooter.jsx
@@ -56,7 +56,7 @@ const MainFooter = () => {
   const socialLinks = [
     { 
       name: 'Facebook', 
-      href: 'https://facebook.com', 
+      href: 'https://www.facebook.com/share/1ATYxa8bBS/?mibextid=wwXIfr', 
       icon: <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z" />
     },
     { 
@@ -76,13 +76,11 @@ const MainFooter = () => {
       )
     },
     { 
-      name: 'LinkedIn', 
-      href: 'https://linkedin.com', 
+      name: 'WhatsApp', 
+      href: 'https://chat.whatsapp.com/JV4gFARWHqU9oYpRSWiZFt', 
       icon: (
         <>
-          <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z" />
-          <rect x="2" y="9" width="4" height="12" />
-          <circle cx="4" cy="4" r="2" />
+          <path d="M21 11.5a8.38 8.38 0 0 1-.9 3.8 8.5 8.5 0 0 1-7.6 4.7 8.38 8.38 0 0 1-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 0 1-.9-3.8 8.5 8.5 0 0 1 4.7-7.6 8.38 8.38 0 0 1 3.8-.9h.5a8.48 8.48 0 0 1 8 8v.5z" />
         </>
       )
     },
